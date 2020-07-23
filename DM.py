d046a20117435ff367d79700fe5b2cb736c05de1
sjha80 <68658126+sjha80@users.noreply.github.com>
2020-07-23 20:20:08 +0900

    Update DM.py

--- DM.py
+++ DM.py
@@ -2,13 +2,13 @@ import discord
 import asyncio
 import datetime
 
-token = "토큰"
+token = "NzM1ODE3OTIzMjU1NDY4MTI0.XxlyQA.72XzXuZ8mItpXv1dMzMgZb2HUDI"
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('구문') #<- 봇 상태 구문
+    game = discord.Game('문의 : ! 연성#3527') #<- 봇 상태 구문
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -21,10 +21,10 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == 677902081193017395: #<-본인 디코아이디 기재 (사용할 유저등록)
+                    if message.author.id == 510440718217642001: #<-본인 디코아이디 기재 (사용할 유저등록)
                         embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="공지") 
-                        embed.add_field(name="카키 디자인", value=msg, inline=True)
-                        embed.set_footer(text=f"현우#7324")
+                        embed.add_field(name="! 연성", value=msg, inline=True)
+                        embed.set_footer(text=f"! 연성#3527")
                         await i.send(embed=embed)
                 except:
                     pass
